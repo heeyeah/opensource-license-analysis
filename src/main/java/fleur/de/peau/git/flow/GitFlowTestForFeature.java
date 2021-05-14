e5f8f36f7de105c667bc793c74074a0c8d2edf9e
heeyeah <gdshy221@gmail.com>
2021-05-14 16:12:44 +0900

    Another commit!

--- src/main/java/fleur/de/peau/git/flow/GitFlowTestForFeature.java
+++ src/main/java/fleur/de/peau/git/flow/GitFlowTestForFeature.java
@@ -2,10 +2,17 @@ package fleur.de.peau.git.flow;
 
 public class GitFlowTestForFeature {
 
-    public void forFeature() {
+    public void forFeature(String platform) {
 
         boolean isScrollLock = true;
         System.out.println(!isScrollLock + "hello, world!");
         System.out.println(isScrollLock);
+
+
+        if(platform.equals("OPENSHIFT")) {
+            System.out.println("platform is ...!");
+        }
+
+
     }
 }
